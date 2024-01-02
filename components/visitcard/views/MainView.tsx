630e3f03941d08413c467d73d5ae7264c9d2047d
amkarkhi <amkarkhi@gmail.com>
2024-01-03 00:23:23 +0330

    remove dark and light one and keep the only dark themed version

--- components/visitcard/views/MainView.tsx
+++ components/visitcard/views/MainView.tsx
@@ -1,21 +1,25 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren } from "react";
 
 const MainView = (props: PropsWithChildren) => {
-   return (
-
-      <div className="w-screen h-screen flex flex-col items-center justify-between overflow-hidden">
-         <div
-            className="absolute inset-0 w-full h-2/3 flex flex-col items-center justify-center rounded-s overflow-hidden">
-            {props.children}
-         </div>
-         <div style={{ transform: "rotateX(130deg) skewX(0deg) skewY(0deg)", bottom: "-7.4%" }}
-            className="absolute h-1/2 w-full flex flex-col items-center justify-center blur-sm overflow-hidden bg-black">
-            {props.children}
-         </div>
+  return (
+    <div className='w-screen h-screen flex flex-col items-center justify-between overflow-hidden'>
+      <div
+        className='absolute inset-0 w-full h-2/3 flex flex-col items-center justify-center rounded-s overflow-hidden'
+        style={{ pointerEvents: "none" }}
+      >
+        {props.children}
       </div>
+      <div
+        style={{
+          transform: "rotateX(130deg) skewX(0deg) skewY(0deg)",
+          bottom: "-7.4%",
+        }}
+        className='absolute h-1/2 w-full flex flex-col items-center justify-center blur-sm overflow-hidden bg-black'
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
-
-   )
-}
-
-export default MainView
+export default MainView;
