3df7a17a21d6fd5a763c1a7ef7dcfd9dfca18ccc
Amin Karkhi <amkarkhi@gmail.com>
2025-05-03 22:00:18 +0330

    add game of life

--- app/wasm/page.tsx
+++ app/wasm/page.tsx
@@ -1,24 +1,57 @@
 "use client";
 
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as wasm from "@wasm/gameoflife";
-import { useMirrored } from "@/components/visitcard/views/MainView";
+// import { useMirrored } from "@/components/visitcard/views/MainView";
 
 const Page = () => {
-  const isMirrored = useMirrored();
+  // const isMirrored = useMirrored();
+  const canvasRef = useRef<HTMLPreElement>(null);
+  const [canvas, setCanvas] = useState<wasm.Universe | null>(null);
 
   useEffect(() => {
-    if (isMirrored) {
-      wasm
-        .default()
-        .then(() => {
-          wasm?.greet("Amin");
-        })
-        .catch(console.log);
-    }
-  }, [isMirrored]);
+    wasm
+      .default()
+      .then(() => {
+        const universe = wasm.Universe.new(66, 20);
+        setCanvas(universe);
+      })
+      .catch(console.log);
+  }, []);
 
-  return <div>{isMirrored ? "test" : "ffffffffffffffffffffffff"}</div>;
+  const renderloop = useCallback(() => {
+    if (canvasRef?.current && canvas) {
+      canvasRef.current.textContent = canvas.render();
+      canvas.tick();
+      requestAnimationFrame(renderloop);
+    }
+  }, [canvas, canvasRef]);
+
+  useEffect(renderloop, [renderloop]);
+
+  return (
+    <div
+      style={{
+        fontSize: "1em",
+        overflow: "hidden",
+        border: "1px solid white",
+        filter: "blur(1px)",
+      }}
+    >
+      <pre
+        style={{
+          width: "40em",
+          height: "30em",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        ref={canvasRef}
+      >
+        test
+      </pre>
+    </div>
+  );
 };
 
 export default Page;
---
+++ public/pkg/gameoflife_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_universe_free: (a: number, b: number) => void;
+export const universe_tick: (a: number) => void;
+export const universe_new: (a: number, b: number) => number;
+export const universe_render: (a: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
